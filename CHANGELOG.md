7aba1ee331c159796c0a05596cd7fc387281ac37
Nick Sutterer <apotonick@gmail.com>
2022-12-13 18:34:18 +0100

    releasing 0.0.2.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,6 @@
-## [Unreleased]
+## [0.0.2]
+
+* Require `forwardable`.
 
 ## [0.0.1] - 2022-12-13
 
--- lib/trailblazer/core/utils/version.rb
+++ lib/trailblazer/core/utils/version.rb
@@ -1,7 +1,7 @@
 module Trailblazer
   module Core
     module Utils
-      VERSION = "0.0.1"
+      VERSION = "0.0.2"
     end
   end
 end

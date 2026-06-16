bb71e50ac6be5453854000ee4028b9a7aaac0843
Nick Sutterer <apotonick@gmail.com>
2026-06-16 17:56:25 +0200

    allow passing :circuit_options in #assert_run.

--- lib/trailblazer/core/utils/assert_run.rb
+++ lib/trailblazer/core/utils/assert_run.rb
@@ -1,25 +1,25 @@
 module Trailblazer
   module Core::Utils
     module AssertRun
-      def assert_run(circuit, node: false, terminus: nil, seq:, flow_options: {}, application_ctx: {}, **lib_ctx)
+      def assert_run(circuit, node: false, terminus: nil, seq:, flow_options: {}, application_ctx: {}, signal: nil, circuit_options: {}, **lib_ctx)
         runner = Trailblazer::Circuit::Node::Runner
 
         # If circuit isn't a Node instance already, wrap it in a "canonical node".
         canonical_node = node ? circuit : Trailblazer::Circuit::Node[:my_canonical, circuit, Trailblazer::Circuit::Processor]
 
-        circuit_options = {}
         circuit_options = circuit_options.merge(runner: runner)
         circuit_options = circuit_options.merge(context_implementation: Trailblazer::Circuit::Context) # FIXME: remove
 
         flow_options = {application_ctx: {seq: [], **application_ctx}, **flow_options}
+        lib_ctx, flow_options, signal = runner.(canonical_node, lib_ctx, flow_options, signal, **circuit_options)
 
-        lib_ctx, flow_options, signal = runner.(canonical_node, lib_ctx, flow_options, nil, **circuit_options)
-
-        assert_equal signal, terminus
-        assert_equal flow_options[:application_ctx][:seq], seq # FIXME: test all ctx variables.
+        assert_equal signal, terminus, "Expected terminus #{terminus.inspect} does not match actual #{signal.inspect}"
+        assert_equal flow_options[:application_ctx][:seq], seq, ":seq does not match" # FIXME: test all ctx variables.
 
         return lib_ctx, flow_options, signal
       end
     end
   end
 end
+
+# FIXME: test :circuit_options kw
